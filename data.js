85c76acab78edbb9255d15ac00e7d25dd221237c
Wes Bos <wesbos@gmail.com>
2020-01-06 15:41:12 -0500

    testing

---
+++ data.js
@@ -0,0 +1,50 @@
+// Add yourself. Insert an object at any point - it doesn't matter if you go before someone else as results are randomized.
+const pages = [
+  {
+    name: 'Wes Bos',
+    // Short description
+    description: 'Web Developer, Tutorial Maker, Podcaster, BBQ Lover',
+    // URL to your /uses page
+    url: 'https://wesbos.com/uses',
+    // An emoji that describes you
+    emoji: '🔥',
+    // emoji of your country's flag
+    country: '🇨🇦',
+    // apple, windows or linux
+    computer: 'apple',
+    // apple or android
+    phone: 'apple',
+    // Tags
+    // Dev Tags: Engineer, Developer, Designer, Front End, Back End, Full Stack,
+    // Other: Tags: Entrepreneur, Teacher, Podcaster, YouTuber, Blogger, Speaker,
+    // Language Tags: JavaScript, PHP, Rails, Ruby, TypeScript...
+    tags: [
+      'Developer',
+      'Full Stack',
+      'Entrepreneur',
+      'Teacher',
+      'YouTuber,',
+      'JavaScript',
+    ],
+  },
+  {
+    name: 'Wes Bos',
+    // Short description
+    description: 'Web Developer, Tutorial Maker, Podcaster, Bboy',
+    // URL to your /uses page
+    url: 'https://kit.com/leveluptutorials/podcasting-screencasting-gear',
+    // An emoji that describes you
+    emoji: '💪🏻',
+    // emoji of your country's flag
+    country: '🇺🇸',
+    // apple, windows or linux
+    computer: 'apple',
+    // apple or android
+    phone: 'apple',
+    // Tags
+    // Dev Tags: Engineer, Developer, Designer, Front End, Back End, Full Stack,
+    // Other: Tags: Entrepreneur, Teacher, Podcaster, YouTuber, Blogger, Speaker,
+    // Language Tags: JavaScript, PHP, Rails, Ruby
+    tags: ['Developer', 'FrontEnd', 'Entrepreneur', 'Teacher', 'JavaScript'],
+  },
+];
