20b74248ec85414292cc7bef0d0dbbab2ee36b23
Wes Bos <wesbos@gmail.com>
2020-01-09 11:54:53 -0500

    fix russia

--- src/data.js
+++ src/data.js
@@ -618,14 +618,9 @@ const pages = [
     url: 'https://pavel.dev/uses',
     twitter: '@theopract',
     emoji: '🤟',
-    country: 'RU',
+    country: '🇷🇺',
     computer: 'windows',
     phone: 'android',
-    // Tags - You can add your own, but please keep it to one word. "Social vape entrepreneur influencer denver" isn't a tag.
-
-    // Dev Tags: Engineer, Developer, Designer, Front End, Back End, Full Stack,
-    // Other: Tags: Entrepreneur, Teacher, Podcaster, YouTuber, Blogger, Speaker,
-    // Language Tags: JavaScript, PHP, Rails, Ruby, TypeScript, React, Vue, Node
     tags: [
       'Developer',
       'Front End',
