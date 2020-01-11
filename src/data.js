7ff404231038f1a47c0ba76a5776549673c12d7c
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2020-01-11 09:43:52 -0300

    FMG's data

--- src/data.js
+++ src/data.js
@@ -2064,6 +2064,38 @@ const pages = [
       'YouTuber',
     ],
   },
+  {
+    name: 'Facundo Gordillo',
+    // Short description
+    description:
+      'Dev Architect. Entrepreneur, Frustrated Youtuber, Anime freak',
+    // URL to your /uses page
+    url: 'https://facundogordillo.com/uses',
+    twitter: '@FMGordillo',
+    // An emoji that describes you
+    emoji: '💸',
+    // emoji of your country's flag
+    country: 'ar',
+    // ONE of: apple, windows or linux
+    computer: 'windows',
+    // iphone or android
+    phone: 'android',
+    // Tags - You can add your own, but please keep it to one word. "Social vape entrepreneur influencer denver" isn't a tag.
+
+    // Dev Tags: Engineer, Developer, Designer, Front End, Back End, Full Stack,
+    // Other: Tags: Entrepreneur, Teacher, Podcaster, YouTuber, Blogger, Speaker,
+    // Language Tags: JavaScript, PHP, Rails, Ruby, TypeScript, React, Vue, Node
+    tags: [
+      'Developer',
+      'Full Stack',
+      'Entrepreneur',
+      'YouTuber',
+      'JavaScript',
+      'React',
+      'Node',
+      'IBM',
+    ],
+  },
 ];
 
 export default pages;
