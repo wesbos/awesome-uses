6c94902884177f9f1c5d09e11422e4c6c0a797f3
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-01-21 18:03:05 +0000

    comment on PR after validation run.

--- scripts/data-validate.js
+++ scripts/data-validate.js
@@ -1,8 +1,35 @@
 const core = require('@actions/core');
+const github = require('@actions/exec');
 const { getMasterData, Schema, getStatusCode } = require('./utils.js');
 const srcData = require('../src/data.js');
 
-(async () => {
+async function commentPullRequest(errors, failedUrls /* , imagePath */) {
+  let comment = '';
+  if (errors.length || failedUrls.length) {
+    core.setFailed('Action failed with errors, see logs & comment');
+
+    comment += 'Fix the following issues: ';
+    comment += errors.map(e => e.message).join('\n');
+    comment += failedUrls.join('\n');
+  } else {
+    comment += 'No validation issues detected.';
+  }
+
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const { context } = github;
+  if (!GITHUB_TOKEN || !context.payload.pull_request) return;
+
+  const pullRequestNumber = context.payload.pull_request.number;
+
+  const octokit = new github.GitHub(GITHUB_TOKEN);
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pullRequestNumber,
+    body: comment,
+  });
+}
+
+async function main() {
   // on master branch will be empty array
   const masterDataUrls = (await getMasterData()).map(d => d.url);
   // so here data will be an array with all users
@@ -18,25 +45,21 @@ const srcData = require('../src/data.js');
     e.details.forEach(d => core.error(d.message));
   });
 
-  let failedUrlsCount = 0;
+  const failedUrls = [];
   for (const { url } of data) {
     try {
       const statusCode = await getStatusCode(url);
       if (statusCode < 200 || statusCode >= 400) {
         core.error(`Ping to "${url}" failed with status: ${statusCode}`);
-        failedUrlsCount += 1;
+        failedUrls.push(url);
       }
     } catch (e) {
       core.error(`Ping to "${url}" failed with error: ${e}`);
-      failedUrlsCount += 1;
+      failedUrls.push(url);
     }
   }
 
-  if (failedUrlsCount) {
-    core.error(`Action failed with ${failedUrlsCount} URL fetch failures`);
-  }
+  await commentPullRequest(errors, failedUrls, '');
+}
 
-  if (errors.length || failedUrlsCount) {
-    core.setFailed('Action failed with errors, see logs');
-  }
-})();
+main();
