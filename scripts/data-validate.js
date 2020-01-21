611fb0c595c1dc4ccfd5ecf7fd880fea2c321813
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-01-21 18:16:44 +0000

    fix wrong import

--- scripts/data-validate.js
+++ scripts/data-validate.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('@actions/exec');
+const github = require('@actions/github');
 const { getMasterData, Schema, getStatusCode } = require('./utils.js');
 const srcData = require('../src/data.js');
 
