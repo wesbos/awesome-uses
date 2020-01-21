764c5e3df0be0955ecb0b5cdc0470ed5e4cf6b03
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-01-21 18:52:21 +0000

    change my URL so it appears in the message

--- src/data.js
+++ src/data.js
@@ -1814,7 +1814,7 @@ module.exports = [
     name: 'Hugo Di Francesco',
     description:
       "JavaScript developer, blogger at codewithhugo.com, co-author of 'Professional JavaScript' with Packt.",
-    url: 'https://codewithhugo.com/uses/',
+    url: 'https://codewithhugo.com/uses',
     twitter: '@hugo__df',
     emoji: '👓',
     country: '🇬🇧',
