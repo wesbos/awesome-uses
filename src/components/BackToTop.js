31616490e0a704ae314ffa9247028065c7eaa84c
Mina Saleeb <saleebmina@icloud.com>
2020-01-27 10:14:27 -0500

    add scroll to top compatibility with other browsers

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -24,6 +24,14 @@ const BackToTopLink = styled.a`
   }
 `;
 
+function scrollTopMax() {
+  let ref;
+  // scrollTopMax is only on Firefox right now! https://caniuse.com/#search=scrolltopmax
+  return (ref = document.scrollingElement.scrollTopMax) != null
+    ? ref
+    : document.scrollingElement.scrollHeight - document.documentElement.clientHeight
+}
+
 function useScrollPosition() {
   const [percent, setPercent] = useState(0);
 
@@ -31,7 +39,7 @@ function useScrollPosition() {
     console.log(document.documentElement.scrollTop);
     const howFar =
       document.documentElement.scrollTop /
-      document.documentElement.scrollTopMax;
+      scrollTopMax();
     setPercent(howFar);
   }
 
