e8dc0806d4c1f4fa5352a04b8780aefd055fc0c1
Dhaval-dinc <dhaval@dhanashree.com>
2020-02-12 15:07:02 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,6 +1,17 @@
 // keep it commonjs export
 module.exports = [
   {
+    name: 'Dhaval Boraniya',
+    description: 'Developer',
+    url: 'http://itca.xyz/uses',
+    twitter: '@dhavalboraniya',
+    emoji: '😇',
+    country: 'india',
+    computer: 'intel',
+    phone: 'android',
+    tags: ['Developer', 'PHP'],
+  },
+  {
     name: 'Dhanish Gajjar',
     description: 'Developer',
     url: 'https://dhanishgajjar.com/uses',
