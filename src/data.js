73c4312cc560ca6bf7cd36c83ec1c88350232ec6
Mahmoud Ashraf <mahmoudss1997@gmail.com>
2020-02-24 23:40:08 +0200

    Add @_22mahmoud (#675)
    * add mahmoudashraf.dev uses
    
    * fix my name typo

--- src/data.js
+++ src/data.js
@@ -2546,6 +2546,24 @@ module.exports = [
     tags: ['Developer', 'Full Stack', 'Entrepreneur', 'Blogger', 'JavaScript'],
   },
   {
+    name: 'Mahmoud Ashraf',
+    description: 'Front-End Developer, sometimes do backend stuff.',
+    url: 'http://mahmoudashraf.dev/uses',
+    twitter: '@_22mahmoud',
+    emoji: '🧠',
+    country: '🇪🇬',
+    computer: 'linux',
+    phone: 'android',
+    tags: [
+      'JavaScript',
+      'TypeScript',
+      'React',
+      'Node',
+      'GraphQl',
+      'Svelte'
+    ],
+  },
+  {
     name: 'Pouria Ezzati',
     description: 'Web developer. Digs music, football and a e s t h e t i c s',
     url: 'https://pouria.dev/uses',
