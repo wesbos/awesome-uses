cc331fdc8612d8621d3d696663467fde113c4295
MadebyToast <59581352+MadebyToast@users.noreply.github.com>
2020-04-25 16:25:21 +0100

    Update data.js with Keith Donegan (#768)
    * Update data.js
    
    Adding myself to this (hopefully)!
    
    * Removed extra character

--- src/data.js
+++ src/data.js
@@ -1302,6 +1302,23 @@ module.exports = [
     ],
   },
   {
+    name: 'Keith Donegan',
+    description:
+      'Irish WordPress developer, based in London, UK.',
+    url: 'https://www.keithdonegan.com/uses/',
+    twitter: '@eirestudio',
+    emoji: '🤪',
+    country: '🇮🇪',
+    computer: 'apple',
+    phone: 'iphone',
+    tags: [
+      'WordPress',
+      'Developer',
+      'CSS',
+      'Front-End'
+    ],
+  },
+  {
     name: 'Makon Cline',
     description:
       'Engineer, Developer, Home Cook. I like to make useful things and share them with others.',
