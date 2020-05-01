b818fd80aa2e1582dbbdfe292f676b8e28dfe45d
Alex Duval <alexduval71@gmail.com>
2020-05-01 15:04:49 +0200

    Add Alex DUVAL (#789)
    * Add my self
    
    Add ALEX DUVAL
    
    * fix flag

--- src/data.js
+++ src/data.js
@@ -1,5 +1,26 @@
 // keep it commonjs export
 module.exports = [
+    {
+    name: 'Alex Duval',
+    description: 'Fullstack Dev, Teacher, Freeride skier',
+    url: 'https://www.alexduval.fr/uses',
+    twitter: '@xlanex6',
+    emoji: '🎿',
+    country: '🇫🇷',
+    computer: 'apple',
+    phone: 'iphone',
+    tags: [
+      'Developer',
+      'Full Stack',
+      'Web',
+      'jamSTACK',
+      'Vue',
+      'Nuxt',
+      'JavaScript',
+      'TailwindCSS',
+      'Vuetify',
+    ],
+  },
   {
     name: 'Felix Yeboah Jefferson',
     description: 'Fullstack Developer, UI Designer & a Nomad',
