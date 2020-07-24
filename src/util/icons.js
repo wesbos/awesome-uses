54a353638c504849c43c2945a2077c51ab306858
Jérémie Bertrand <jeremie.bertrand@outlook.fr>
2020-07-24 15:06:13 +0200

    Add my profile and windowsphone icon

--- src/util/icons.js
+++ src/util/icons.js
@@ -1,7 +1,8 @@
 import iphone from '../images/iphone.png';
 import android from '../images/android.png';
+import windowsphone from '../images/windowsphone.png';
 import windows from '../images/windows.svg';
 import apple from '../images/apple.svg';
 import linux from '../images/linux.png';
 
-export { iphone, android, windows, apple, linux };
+export { iphone, android, windowsphone, windows, apple, linux };
