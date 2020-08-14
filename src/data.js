80a5852dd8dc29629eb4bbcbb5294544e4f6ab7d
Quentin <qlaffont@users.noreply.github.com>
2020-08-14 12:00:05 +0200

    add Quentin Laffont (#914)
    * add Quentin Laffont
    
    * Update data.js
    
    * Update data.js
    
    * Update data.js

--- src/data.js
+++ src/data.js
@@ -7064,6 +7064,30 @@ module.exports = [
     ],
   },
   {
+    name: 'Quentin Laffont',
+    description:
+      'Full Stack JS Developer, Tournament Organiser, Video-Game lover',
+    url: 'https://uses.qlaffont.com',
+    twitter: '@qlaffont',
+    emoji: '😎',
+    country: '🇫🇷',
+    computer: 'apple',
+    phone: 'android',
+    tags: [
+      'Engineer',
+      'Back End',
+      'Front End',
+      'Full Stack',
+      'Entrepreneur',
+      'JavaScript',
+      'TypeScript',
+      'React',
+      'Angular',
+      'CSS',
+      'SCSS',
+    ],
+  },
+  {
     name: 'José Carlos Correa',
     description:
       'Fullstack developer and speaker. Lead teacher @ironhack and gatsby teacher @platzi. Learning all the time.',
