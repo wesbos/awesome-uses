aff3affc2fbd85b20a2f8f3e7152f32720575089
Quentin <qlaffont@users.noreply.github.com>
2020-08-14 10:18:44 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -7043,7 +7043,7 @@ module.exports = [
   {
     name: 'Quentin Laffont',
     description:
-      'Full Stack JS Developer & Gamer',
+      'Full Stack JS Developer, Tournament Organiser, Video-Game lover',
     url: 'https://uses.qlaffont.com',
     twitter: '@qlaffont',
     emoji: '😎',
