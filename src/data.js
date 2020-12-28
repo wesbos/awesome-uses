d8c66ad1e40d22636ac76f2c1322f03f1d953bb8
Matthäus Mzyk <mathaeus.mzyk@hotmail.com>
2020-12-28 21:13:30 +0100

    Add Matthäus Mzyk

--- src/data.js
+++ src/data.js
@@ -2,6 +2,24 @@
 // Data is validated against the following schema https://github.com/wesbos/awesome-uses/blob/master/scripts/utils.js#L6-L21
 module.exports = [
   {
+    name: "Matthäus Mzyk",
+    description: "T-Shaped Full Stack Developer with strong focus on Frontend",
+    url: "https://matthaeusmzyk.github.io/uses",
+    emoji: "👨‍🚀",
+    country: "🇩🇪",
+    computer: "apple",
+    phone: "iphone",
+    tags: [
+      "Typescript",
+      "Angular",
+      "React",
+      "Node.js",
+      "Azure",
+      "Full Stack",
+      "Front End",
+    ],
+  },
+  {
     name: 'Simon Smale',
     description: 'Full Stack Developer and beginner digital gardener',
     url: 'https://github.com/SSmale/uses',
