92c124d8acf1ac12ebf6a0c95ac75d63af468879
Henrik Nyh <henrik@nyh.se>
2021-04-02 20:19:44 +0100

    Add myself (Henrik Nyh)

--- src/data.js
+++ src/data.js
@@ -2418,6 +2418,23 @@ module.exports = [
       'Web',
     ],
   },
+    {
+    name: 'Henrik Nyh',
+    description: 'Swedish web developer in Yorkshire, UK.',
+    url: 'https://henrik.nyh.se/uses',
+    twitter: '@henrik',
+    emoji: '🦆',
+    country: '🇬🇧',
+    computer: 'apple',
+    phone: 'iphone',
+    tags: [
+      'Developer',
+      'Blogger',
+      'Ruby',
+      'Elixir',
+      'Vim',
+    ],
+  },
   {
     name: 'Manoj Barman',
     description: 'Working hardly, or Hardly working..',
