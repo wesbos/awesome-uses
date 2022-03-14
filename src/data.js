4270e21f6825d858abaabaab494d064e214b7911
Erv Walter <erv@ewal.net>
2022-03-14 18:21:20 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -10080,7 +10080,7 @@ module.exports = [
   {
     name: 'Erv Walter',
     description: 'Father, Husband, Web Developer, Board Game Addict',
-    url: 'https://blog.ewal.net/uses/',
+    url: 'https://ewal.dev/uses',
     twitter: '@ervwalter',
     emoji: '🎲',
     country: '🇺🇸',
@@ -10092,7 +10092,7 @@ module.exports = [
       'TypeScript',
       'React',
       'C#',
-      '.NET Core',
+      '.NET',
       'Azure',
     ],
   },
