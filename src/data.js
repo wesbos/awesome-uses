047ab855a34d28233636333d4539d147eae2e97f
Blake Campbell <blakecampbell.dev@gmail.com>
2022-08-14 21:09:01 -0400

    Removing Blaise Sebagabo: Site not found

--- src/data.js
+++ src/data.js
@@ -8821,27 +8821,6 @@ module.exports = [
     tags: ['Developer', 'UX Designer', 'Engineer', 'JavaScript', 'CSS'],
   },
   {
-    name: 'Blaise Sebagabo',
-    description:
-      'Full Stack Developer, designer, and web developer from Edmonton, Canada',
-    url: 'https://seblaise.dev/uses',
-    twitter: '@BlaiseSebagabo',
-    emoji: '🗾',
-    country: '🇨🇦',
-    computer: 'apple',
-    phone: 'iphone',
-    tags: [
-      'Developer',
-      'Full Stack',
-      'React',
-      'C#',
-      '.NET',
-      'JAVA',
-      'CSS',
-      'Python',
-    ],
-  },
-  {
     name: 'Caleb Ukle',
     description:
       "I'm a software developer, technology enthusiast, and simply enjoy learning new things.",
