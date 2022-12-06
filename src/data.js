b9d2a7f2c6dd6bd8a20b9f72d3289927d8880412
Ryan <ryan.freeman@aerlingus.com>
2022-12-06 14:49:31 +0000

    Fix country code

--- src/data.js
+++ src/data.js
@@ -214,7 +214,7 @@ module.exports = [
     description: 'Full-stack software engineer from Dublin, Ireland',
     url: 'https://ryanfreeman.dev/uses',
     twitter: '@ryanfreeman_dev',
-    country: 'ie',
+    country: '🇮🇪',
     emoji: '✈️',
     computer: 'windows',
     phone: 'iphone',
