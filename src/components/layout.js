f7936d2e50d3863e0c82bbd478515754bd7693e9
Wes Bos <wesbos@gmail.com>
2023-02-21 14:34:15 -0500

    hydration warning

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,34 +5,34 @@ import 'normalize.css';
 
 export default function Layout({ children }) {
   return (
-      <main className="Main">
-        <Header />
-        {children}
-        <footer>
-          <center ya-i-used-a-center-tag="sue me">
-            <p>
-              Made by <a href="https://wesbos.com">Wes Bos</a> with{' '}
-              <a href="https://www.remix.run">Remix</a> ©{' '}
-              {new Date().getFullYear()}
-            </p>
-            <p>
-              Source on{' '}
-              <a href="https://github.com/wesbos/awesome-uses/">GitHub</a>. Add
-              yourself!
-            </p>
-            <p>
-              Icons from <a href="https://icons8.com">icons8.com</a>
-            </p>
-            <p>
-              Domain provided by <a href="https://get.tech/">.Tech</a>
-            </p>
-            <p>
-              Hosted on <a href="https://netlify.com">Netlify</a>
-            </p>
-            <p>Rendered Fresh</p>
-          </center>
-        </footer>
-      </main>
+    <main className="Main">
+      <Header />
+      {children}
+      <footer>
+        <center ya-i-used-a-center-tag="sue me">
+          <p>
+            Made by <a href="https://wesbos.com">Wes Bos</a> with{" "}
+            <a href="https://www.remix.run">Remix</a> ©{" "}
+            {new Date().getFullYear()}
+          </p>
+          <p>
+            Source on{" "}
+            <a href="https://github.com/wesbos/awesome-uses/">GitHub</a>. Add
+            yourself!
+          </p>
+          <p>
+            Icons from <a href="https://icons8.com">icons8.com</a>
+          </p>
+          <p>
+            Domain provided by <a href="https://get.tech/">.Tech</a>
+          </p>
+          <p>
+            Hosted on <a href="https://netlify.com">Netlify</a>
+          </p>
+          <p suppressHydrationWarning>Rendered Fresh</p>
+        </center>
+      </footer>
+    </main>
   );
 };
 
