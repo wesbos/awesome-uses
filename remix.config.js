a9fd7d5d1362ea3f379f051e8d8607bf92a243a8
Wes Bos <wesbos@gmail.com>
2023-02-21 15:37:44 -0500

    Netlify Edge Functions

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,7 @@
+const { config } = require("@netlify/remix-edge-adapter");
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  ...config,
   appDirectory: "src",
   future: {
     unstable_postcss: true,
@@ -9,5 +11,5 @@ module.exports = {
     process.env.NETLIFY || process.env.NETLIFY_LOCAL
       ? "./server.js"
       : undefined,
-  serverBuildPath: ".netlify/functions-internal/server.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
 };
