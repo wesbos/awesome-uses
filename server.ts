53706076ddca47d0acac5bc46af32bdf311df2f0
Wes Bos <wesbos@gmail.com>
2023-02-22 15:32:40 -0500

    hmm

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 // Import path interpreted by the Remix compiler
 import * as build from "@remix-run/dev/server-build";
-import { createRequestHandler } from "@netlify/remix-edge-adapter";
+// import { createRequestHandler } from "@netlify/remix-edge-adapter";
+import { createRequestHandler } from "./netlify-server";
 
 export default createRequestHandler({
   build,
