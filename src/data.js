177a8a6dbcf1223c7d851d85002ca2469a806995
Jason Scheirer <jason.scheirer@gmail.com>
2024-12-13 16:03:37 -0800

    Add @jason@computering.pro

--- src/data.js
+++ src/data.js
@@ -13696,6 +13696,29 @@ module.exports = [
     ],
   },
   {
+    name: 'Jason Scheirer',
+    description:
+      'Software Engineer, Systems Enthusiast. I can probably solve that problem quickly.',
+    url: 'https://jasonscheirer.com/uses/',
+    bluesky: 'jasonscheirer.com',
+    mastodon: '@jason@computering.pro',
+    emoji: '🦨',
+    country: '🇺🇸',
+    computer: 'linux',
+    phone: 'iphone',
+    tags: [
+      'Engineer',
+      'Python',
+      'Golang',
+      'FastAPI',
+      'Ruby on Rails',
+      'JavaScript',
+      'React',
+      'SolidJS',
+      'PostgreSQL',
+    ],
+  },
+  {
     name: 'Frédéric Harper',
     description:
       'Developer Advocate with a knack for public speaking & making videos',
