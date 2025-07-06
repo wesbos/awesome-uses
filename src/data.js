3ea88f073812e73351c92a19792dce1570aba003
Manthan Ankolekar <manthan.ank46@gmail.com>
2025-07-06 13:02:01 +0530

    chore: Update data.js

--- src/data.js
+++ src/data.js
@@ -18761,8 +18761,8 @@ module.exports = [
   {
     name: 'Manthan Ank',
     description:
-      "I'm Front-End Developer, Intermediate Learner, Also as a hobby I write blogs related to Web Development.",
-    url: 'https://manthanank.web.app/uses',
+      "I'm Software Developer, Intermediate Learner, Also as a hobby I write blogs related to Web Development.",
+    url: 'https://manthanank.github.io/uses',
     twitter: '@manthanank',
     emoji: '🧑‍💻',
     country: '🇮🇳',
@@ -18779,6 +18779,12 @@ module.exports = [
       'MongoDB',
       'Firebase',
       'GraphQL',
+      'Python',
+      'MySQL',
+      'Supabase',
+      'PostgreSQL',
+      'Nestjs',
+      'Docker'
     ],
   },
   {
