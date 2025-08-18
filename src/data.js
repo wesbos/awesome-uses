788f0a23197a4ecce0fbefec40268c8d1cbb86f3
Manish Tamang <126256480+Manish-Tamang@users.noreply.github.com>
2025-08-18 21:40:17 +0545

    Adding Manish Tamang

--- src/data.js
+++ src/data.js
@@ -981,6 +981,26 @@ module.exports = [
       'Remote',
     ],
   },
+    {
+    name: 'Manish Tamang',
+    description: 'Young fullstack web app developer',
+    url: 'https://www.manishtamang.com/uses',
+    emoji: '🐸',
+    country: 'NP',
+    computer: 'windows',
+    phone: 'android',
+    bluesky: 'manishtamang.com',
+    twitter: '@Manishtamangxyz',
+    tags: [
+      'TypeScript',
+      'JavaScript',
+      'Next.js',
+      'React.js',
+      'Node.js',
+      'TailwindCSS',
+      'Prisma',
+    ],
+  },
   {
     name: 'Gaurav Kesh Roushan',
     description:
