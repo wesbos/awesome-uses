f515adc77cdc46f0b00ca0b8230d23b5a1f9568f
Ruben Arakelyan <ruben@arakelyan.uk>
2026-05-21 13:19:12 +0100

    Delete Ruben Arakelyan's profile from data.js
    Removed entry for Ruben Arakelyan from data.

--- src/data.js
+++ src/data.js
@@ -21755,17 +21755,6 @@ module.exports = [
     ],
   },
   {
-    name: 'Ruben Arakelyan',
-    twitter: '@wackomenace_',
-    description: 'Senior Ruby developer',
-    url: 'https://www.wackomenace.co.uk/uses',
-    emoji: '💾',
-    country: '🇬🇧',
-    computer: 'apple',
-    phone: 'iphone',
-    tags: ['Ruby', 'Ruby on Rails', 'JavaScript', 'Astro'],
-  },
-  {
     name: 'Tim Veletta',
     description: 'Australian full-stack developer and designer',
     url: 'https://www.timveletta.com/uses',
