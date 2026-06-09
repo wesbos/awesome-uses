ab31ac3058b107cacf3aaaaa96eebb72db72f44d
Blake Campbell <blakecampbell.dev@gmail.com>
2026-06-09 11:28:46 -0400

    Removing Henrik Nyh, lapsed url

--- src/data.js
+++ src/data.js
@@ -9991,17 +9991,6 @@ module.exports = [
     ],
   },
   {
-    name: 'Henrik Nyh',
-    description: 'Swedish web developer in Yorkshire, UK.',
-    url: 'https://henrik.nyh.se/uses',
-    twitter: '@henrik',
-    emoji: '🦆',
-    country: '🇬🇧',
-    computer: 'apple',
-    phone: 'iphone',
-    tags: ['Developer', 'Blogger', 'Ruby', 'Elixir', 'Vim'],
-  },
-  {
     name: 'Manoj Barman',
     description: 'Working hardly, or Hardly working..',
     url: 'https://manojbarman.in/uses',
